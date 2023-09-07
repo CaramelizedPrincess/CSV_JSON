ffa4ed2c7d37518038ca60fcb36c81bbf96dbc3b
CaramelizedPrincess <133921403+CaramelizedPrincess@users.noreply.github.com>
2023-09-07 23:32:02 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,106 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json, "data.json");
+
+        String fileNameXML = "data.xml";
+        List<Employee> listXML = parseXML(fileNameXML);
+        String jsonXML = listToJson(listXML);
+        writeString(jsonXML, "data2.json");
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            List<Employee> list = csv.parse();
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String json, String fileNameJSON) {
+        try (FileWriter file = new FileWriter(fileNameJSON)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static List<Employee> parseXML(String fileNameXML) throws ParserConfigurationException, IOException, SAXException {
+        List<String> elements = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileNameXML));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node nodeList1 = nodeList.item(i);
+            if (nodeList1.getNodeName().equals("employee")) {
+                NodeList nodeList2 = nodeList1.getChildNodes();
+                for (int y = 0; y < nodeList2.getLength(); y++) {
+                    Node nodeList3 = nodeList2.item(y);
+                    if (Node.ELEMENT_NODE == nodeList3.getNodeType()) {
+                        elements.add(nodeList3.getTextContent());
+                    }
+                }
+                list.add(new Employee(
+                        Long.parseLong(elements.get(0)),
+                        elements.get(1),
+                        elements.get(2),
+                        elements.get(3),
+                        Integer.parseInt((elements.get(4)))));
+                elements.clear();
+            }
+        }
+        return list;
+    }
+
+}
